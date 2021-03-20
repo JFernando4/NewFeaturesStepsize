06941b8a043c9d349310661a6df8170e82ad155c
jfernando <jfhernan@ualberta.ca>
2021-03-19 18:01:09 -0600

    Updated it to creat 200k transitions and store each seed separately

--- MountainCar_PumpingAction_Trajectories.py
+++ MountainCar_PumpingAction_Trajectories.py
@@ -1,71 +1,73 @@
 import numpy as np
 import pickle
 import os
+import time
+import argparse
 
 from src import MountainCar, Config
 
+NUM_TRANSITIONS = 200001
 
-def GenerateTrajectories(initial_seed=0, num_trajectories=100, num_evaluations=1000, verbose=False):
+
+def GenerateTrajectories(seed=0, num_evaluations=1000, verbose=False):
+
+    data_dir = os.path.join(os.getcwd(), 'mountain_car_prediction_data_{0}evaluations'.format(num_evaluations))
+    os.makedirs(data_dir, exist_ok=True)
+    data_path = os.path.join(data_dir, 'seed'+str(seed)+'.p')
 
     config = Config()
     config.norm_state = True
     epsilon = 0.1
     gamma = 0.99
+    num_states = 2
 
     trajectories = []
     seeds = []
-    for i in range(num_trajectories):
-        print("Currently working on seed {0}...".format(i + initial_seed))
-        np.random.seed(i + initial_seed)
-        # initialize environment
-        env = MountainCar(config)
-        trajectory_states = []
-        trajectory_actions = []
-        trajectory_rewards = []
-        trajectory_terminations = []
-        # generate trajectory
-        terminal = False
+    print("Currently working on seed {0}...".format(seed))
+    np.random.seed(seed)
+    # initialize environment
+    env = MountainCar(config)
+    states = np.zeros((NUM_TRANSITIONS, num_states), dtype=np.float64)
+    actions = np.zeros(NUM_TRANSITIONS, dtype=np.int32)
+    rewards = np.zeros(NUM_TRANSITIONS, dtype=np.int8)
+    terminations = np.zeros(NUM_TRANSITIONS, dtype=np.int8)
+
+    # generate trajectory
+    for i in range(NUM_TRANSITIONS):
         curr_s = env.get_current_state()
-        trajectory_states.append(curr_s)
-        while not terminal:
-            curr_a = pumping_action(curr_s, rprob=epsilon)
-            curr_s, reward, terminal = env.step(curr_a)
-            trajectory_states.append(curr_s)
-            trajectory_actions.append(curr_a)
-            trajectory_rewards.append(reward)
-            trajectory_terminations.append(terminal)
-        env.reset()
-        # compute estimated discounted returns for each state in the trajectory using monte carlo rollouts
-        sample_discounted_returns = np.zeros(len(trajectory_states), dtype=np.float64)
-        for j in range(len(trajectory_states) - 1):
-            sample_discounted_returns[j] += estimate_expected_discounted_return(env=env, init_s=trajectory_states[j],
-                                                                                init_a=trajectory_actions[j],
-                                                                                epsilon=epsilon, gamma=gamma,
-                                                                                samples=num_evaluations)
-        if verbose:
-            for j in range(len(trajectory_states)):
-                print("Step: {0}\tState: {1}\tEstimated Return: {2}".format(j+1, trajectory_states[j],
-                                                                            sample_discounted_returns[j]))
-        trajectories.append(list(zip(trajectory_states, trajectory_actions, trajectory_rewards, trajectory_terminations,
-                                     sample_discounted_returns)))
-        seeds.append(i+initial_seed)
+        curr_a = pumping_action(curr_s, rprob=epsilon)
+        curr_s, reward, terminal = env.step(curr_a)
 
-    trajectories_path = os.path.join(os.getcwd(), 'MountainCar_Trajectories.p')
-    if os.path.isfile(trajectories_path):
-        with open(trajectories_path, mode='rb') as trajectories_file:
-            trajectories_dict = pickle.load(trajectories_file)
-        for i in range(len(seeds)):
-            if seeds[i] not in trajectories_dict['seeds']:
-                trajectories_dict['seeds'].append(seeds[i])
-                trajectories_dict['trajectories'].append(trajectories[i])
-    else:
-        trajectories_dict = {
-            'seeds': seeds,
-            'trajectories': trajectories
-        }
+        states[i] += curr_s
+        actions[i] += np.int32(curr_a)
+        rewards[i] += np.int8(reward)
+        terminations[i] += np.int8(terminal)
 
-    with open(trajectories_path, mode='wb') as trajectories_file:
-        pickle.dump(trajectories_dict, trajectories_file)
+        if terminal:
+            env.reset()
+
+    # compute estimated discounted returns for each state in the trajectory using monte carlo rollouts
+    avg_discounted_returns = np.zeros(NUM_TRANSITIONS, dtype=np.float64)
+    ste_discounted_returns = np.zeros(NUM_TRANSITIONS, dtype=np.float64)
+    for j in range(NUM_TRANSITIONS):
+        if terminations[j] != 1:
+            avg, ste = estimate_expected_discounted_return(env=env, init_s=states[j], init_a=actions[j],
+                                                           epsilon=epsilon, gamma=gamma, samples=num_evaluations)
+            avg_discounted_returns[j] += avg
+            ste_discounted_returns[j] += ste
+
+    if verbose:
+        for j in range(NUM_TRANSITIONS):
+            print("Step: {0}\tState: {1}\tEstimated Return: {2}\tStandard Error {3}".format(j+1,
+                                    states[j], avg_discounted_returns[j], ste_discounted_returns[j]))
+
+    trajectory_dict = {
+        'states': states, 'actions': actions, 'rewards': rewards, 'terminations': terminations,
+        'avg_discounted_return': avg_discounted_returns, 'ste_discounted_returns': ste_discounted_returns
+    }
+
+    with open(data_path, mode='wb') as trajectories_file:
+        pickle.dump(trajectory_dict, trajectories_file)
 
 
 def pumping_action(s: np.ndarray, rprob=0.1):
@@ -93,8 +95,20 @@ def estimate_expected_discounted_return(env: MountainCar, init_s: np.ndarray, in
             current_discounted_return += curr_gamma * curr_reward
             curr_gamma *= gamma
         discounted_returns[i] += current_discounted_return
-    return np.average(discounted_returns)
+    avg = np.average(discounted_returns)
+    ste = np.std(discounted_returns, ddof=1) / np.sqrt(samples)
+    return avg, ste
 
 
 if __name__ == '__main__':
-    GenerateTrajectories(initial_seed=0, num_trajectories=130, num_evaluations=1000, verbose=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-seed', action='store', type=int, default=0)
+    parser.add_argument('-ne', '--num_evaluations', action='store', type=int, default=30)
+    parser.add_argument('-v', '--verbose', action='store_true', default=False)
+    parameters = parser.parse_args()
+
+    start_time = time.time()
+    GenerateTrajectories(seed=parameters.seed, num_evaluations=parameters.num_evaluations, verbose=parameters.verbose)
+    end_time = time.time()
+    total_running_time = (end_time - start_time) / 60
+    print("The total running time was: {0}".format(total_running_time))
